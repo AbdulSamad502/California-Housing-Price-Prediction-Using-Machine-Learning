49d2dbb66adc041ed2b704fd9eb62284f549d568
AbdulSamad502 <samad784600@gmail.com>
2025-11-27 18:48:08 +0530

    Rename 'inputer' to 'imputer' in pipeline

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
-from sklearn.tree import DecisionTreeRegressor
+
 from sklearn.model_selection import cross_val_score
 
 MODEL_FILE="model.pkl"
@@ -18,7 +17,7 @@ PIPELINE_FILE="pipeline.pkl"
 
 def build_pipeline(num_attribs,cat_attribs):
     num_pipeline=Pipeline([
-        ("inputer",SimpleImputer(strategy="median")),
+        ("imputer",SimpleImputer(strategy="median")),
         ("Scalar",StandardScaler())
     ]
     )
